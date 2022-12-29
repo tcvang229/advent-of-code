8d9127a989cbe7d1692832308f9ae7b816ce23bd
tcvang229 <tswjfwmeng@gmail.com>
2022-12-29 17:01:22 -0600

    finished day 2

---
+++ 2022/day2/part1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // calculate all my points
+
+            // determine who wins
+            // add points
+
+            // Counters as in weaknesses, or opponents to lose to.
+            Dictionary<string, string> ShapeCounters = new Dictionary<string, string>()
+            {
+                {"A", "Y"},
+                {"B", "Z"},
+                {"C", "X"},
+                {"X", "B"},
+                {"Y", "C"},
+                {"Z", "A"}
+            };
+
+            // x = rock
+            // y = paper
+            // z = scissors
+
+            // a = rock
+            // b = paper
+            // c = scissors
+
+            Dictionary<string, string> ShapeEqualParts = new Dictionary<string, string>()
+            {
+                {"X", "A"},
+                {"Y", "B"},
+                {"Z", "C"}
+            };
+
+            string puzzleInputPath = "../input.txt";
+            int runningScore = 0;
+            using (StreamReader reader = File.OpenText(puzzleInputPath))
+            {
+                string readText = reader.ReadToEnd();
+
+                string[] gameRounds = readText.Split("\n");
+                Console.WriteLine(gameRounds.Length);
+                foreach (string shapes in gameRounds)
+                {
+                    string[] shapesArray = shapes.Split(" ");
+
+                    if (shapesArray.Length < 2) 
+                    {
+                        continue;
+                    }
+
+                    switch (shapesArray[1])
+                    {
+                        case "X":
+                            runningScore+= 1;
+                            break;
+
+                        case "Y":
+                            runningScore+= 2;
+                            break;
+
+                        case "Z":
+                            runningScore+= 3;
+                            break;
+                        default:
+                            Console.WriteLine("hit");
+                            break;
+                    }
+
+                    if (ShapeEqualParts[shapesArray[1]] == shapesArray[0]) 
+                    {
+                        runningScore+= 3;
+                        continue;
+                    }
+
+                    if (ShapeCounters[shapesArray[1]] != shapesArray[0]) 
+                    {
+                        // Winning case
+                        runningScore+= 6;
+                    }
+                }
+
+                Console.WriteLine(runningScore);
+            }
+        }
+    }
+}
---
+++ 2022/day2/part2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day2 
+{
+    internal class Program
+    {
+        public static Dictionary<string, string> ShapeCounters = new Dictionary<string, string>()
+        {
+            {"A", "paper"},
+            {"B", "scissors"},
+            {"C", "rock"}
+        };
+
+        public static Dictionary<string, int> ResultScores = new Dictionary<string, int>()
+        {
+            {"X", 0},
+            {"Y", 3},
+            {"Z", 6}
+        };
+
+        public static Dictionary<string, int> ShapeScores = new Dictionary<string, int>()
+        {
+            {"A", 1},
+            {"B", 2},
+            {"C", 3}
+        };
+
+        static void Main(string[] args)
+        {
+            // x = lose
+            // y = tie
+            // z = win
+
+            // a = rock
+            // b = paper
+            // c = scissors
+
+            string puzzleInputPath = "../input.txt";
+            int runningScore = 0;
+            using (StreamReader reader = File.OpenText(puzzleInputPath))
+            {
+                string readText = reader.ReadToEnd();
+
+                string[] gameRounds = readText.Split("\n");
+                foreach (string shapes in gameRounds)
+                {
+                    string[] shapesArray = shapes.Split(" ");
+
+                    if (shapesArray.Length < 2) 
+                    {
+                        continue;
+                    }
+
+                    // result winnings
+                    runningScore += ResultScores[shapesArray[1]];
+
+                    // shape winnings
+                    switch (shapesArray[1]) 
+                    {
+                        case "X":
+                            runningScore += GetLosingShapePoints(shapesArray[0]);
+                            break;
+                        case "Y":
+                            runningScore += GetTieShapePoints(shapesArray[0]);
+                            break;
+                        case "Z":
+                            runningScore += GetWinningShapePoints(shapesArray[0]);
+                            break;
+                    }
+                }
+
+                Console.WriteLine(runningScore);
+            }
+        }
+
+        private static int GetLosingShapePoints(string opponentShape) 
+        {
+            switch (opponentShape) 
+            {
+                case "A":
+                    return 3;
+                case "B":
+                    return 1;
+                case "C":
+                    return 2;
+            }
+
+            return 0;
+        }
+
+        private static int GetTieShapePoints(string opponentShape) 
+        {
+            return ShapeScores[opponentShape];
+        }
+
+        private static int GetWinningShapePoints(string opponentShape) 
+        {
+            switch (opponentShape) 
+            {
+                case "A":
+                    return 2;
+                case "B":
+                    return 3;
+                case "C":
+                    return 1;
+            }
+
+            return 0;
+        }
+    }
+}
